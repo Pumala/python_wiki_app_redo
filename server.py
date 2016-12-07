80479ec4029280db3f4eb2a7599f62e3f19910d7
Carolyn Lam <caro_lyn_@hotmail.com>
2016-12-07 12:46:36 -0500

    add comments and hide secret key in .env file

--- server.py
+++ server.py
@@ -14,6 +14,9 @@ db = pg.DB(
     passwd=os.environ.get('PG_PASSWORD')
 )
 
+# App Secret key
+app.secret_key = os.environ.get("SECRET_KEY")
+
 tmp_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask('Wiki', template_folder=tmp_dir)
 
@@ -28,20 +31,34 @@ def render_homepage():
 
 @app.route('/<page_name>')
 def render_page_name(page_name):
+
+    # make a query to grab all the data for the value of page_name
     query = db.query("select page_content.content, page.id as page_id, page_content.id as content_id from page, page_content where page.id = page_content.page_id and page.page_name = $1 order by page_content.id desc limit 1", page_name)
     wiki_page = query.namedresult()
+
+    # set has_content and page_is_taken to False initially
     has_content = False
     page_is_taken = False
+
+    # check the length of the query to see if an entry for it exists in the db
     if len(wiki_page) < 1:
+        # if it doesn't exist, set content to an empty string
         content = ""
     else:
+        # if it does exist, set page_is_taken to True
         page_is_taken = True
+        # assign content the value of content from the query
         content = wiki_page[0].content
+    # check the length of content to see if there's any content
     if len(content) > 0:
+        # if so, assign the value of True
         has_content = True
     else:
+        # else, keep has_content as False and move on
         pass
+    # use markdown and wiki_linkify to update the content
     content = markdown.markdown(wiki_linkify(content))
+
     return render_template(
         'pageholder.html',
         page_is_taken = page_is_taken,
@@ -54,12 +71,17 @@ def render_page_name(page_name):
 
 @app.route('/<page_name>/edit')
 def render_page_edit(page_name):
+    # first check if the user is logged in
     if 'username' in session:
+        # if so, proceed and make a query to grab the content
         query = db.query("select page_content.content from page, page_content where page.id = page_content.page_id and page.page_name = $1 order by page_content.id desc limit 1", page_name)
         wiki_page = query.namedresult()
+        # check the length of the query
         if len(wiki_page) > 0:
+            # if there's content, assign it to content
             content = wiki_page[0].content
         else:
+            # if there isn't, assign it an empty string value
             content = ""
         return render_template(
             'edit_page.html',
@@ -67,6 +89,7 @@ def render_page_edit(page_name):
             content = content
         )
     else:
+        # if user isn't logged in, redirect back to the page
         return redirect('/%s' % page_name)
 
 @app.route('/<page_name>/save', methods=['POST'])
@@ -100,11 +123,14 @@ def save_page_edit(page_name):
 
 @app.route('/search', methods=['POST'])
 def redirect_search():
+    # grab the value of the search keyword
     search = request.form.get('search')
+    # redirect to that search keyword page
     return redirect('/%s' % search)
 
 @app.route('/<page_name>/history')
 def view_page_history(page_name):
+    # make a query to grab all the timestamp, id, and content associated with a particular page
     query = db.query("select page_content.timestamp, page_content.id from page, page_content where page.id = page_content.page_id and page.page_name = $1", page_name)
     page_histories = query.namedresult()
 
@@ -116,7 +142,9 @@ def view_page_history(page_name):
 
 @app.route('/<page_name>/history/record')
 def view_page_record(page_name):
+    # grab the content id
     content_id = request.args.get('id')
+    # make a query to select a specific timestamp, and content from a page
     query = db.query("select page_content.content, page_content.timestamp from page, page_content where page.id = page_content.page_id and page_content.id = $1", content_id)
     page_record = query.namedresult()[0]
 
@@ -128,6 +156,7 @@ def view_page_record(page_name):
 
 @app.route('/all_pages')
 def list_all_pages():
+    # make a querk to grab all the pages that exist in the db
     query = db.query('select page_name from page')
     all_pages = query.namedresult()
     return render_template(
@@ -143,16 +172,20 @@ def new_register():
 
 @app.route('/submit_register_form', methods=['POST'])
 def submit_new_user_auth():
+    # grab the username and password values that the new user submitted
     username = request.form.get('username')
     password = request.form.get('password')
+    # create a new entry in user_auth table in db
     db.insert(
         'user_auth', {
             'username': username,
             'password': password
         }
     )
+    # log the user
     session['username'] = username
-    flash('You have successfully signed up %s!' % username)
+    # flash a message letting the user know they have successfully signed up
+    flash('You have successfully signed up $1!', username)
     return redirect('/')
 
 @app.route('/login')
@@ -163,30 +196,38 @@ def login_user():
 
 @app.route('/submit_login', methods=['POST'])
 def is_logged_in():
+    # grab the username and password values
     username = request.form.get('username')
     password = request.form.get('password')
 
+    # make a query to grab everything connected to the username
     query = db.query("select * from user_auth where username = $1", username)
     user_info = query.namedresult()
     print "USER INFO: %r" % user_info
+    # check the length of the query result
+    # if it's greater than 0, then the user is already a member
     if len(user_info) > 0:
+        # we check if the passwords match
         if user_info[0].password == password:
+            # if so, the user is logged in
             session['username'] = username
+            # display a message informing the user of being logged in
             flash('You have successfully logged in %s!' % username)
+            # redirect back to home page
             return redirect('/')
     else:
         pass
+    # redirect back to login route if user does not exist or passwords did not match
     return redirect('/login')
 
 @app.route('/logout')
 def logout_user():
     username = session['username']
+    # logs the user out of their session
     del session['username']
+    # display a message letting user know they are logged out
     flash('You have successfully logged out %s!' % username)
     return redirect('/')
-
-
-app.secret_key = 'tell your mama that you will be home with bubbly'
 
 if __name__ == '__main__':
     app.run(debug=True)
